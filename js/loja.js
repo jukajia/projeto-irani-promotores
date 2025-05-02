d742830316aec8a6f036e5dc97d32dd0273fb1d1
Juka Produções <82520164+jukajia@users.noreply.github.com>
2025-05-02 12:05:22 -0300

    Update loja.js

--- js/loja.js
+++ js/loja.js
@@ -1,7 +1,7 @@
 const PLANILHAS_LOJAS = {
   '001': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRVQpJV736U_gkzIGzbIdRk4sObA4so3fdj-Emr8WYvd5X20PXr4re_OtEP866H4_LbdJ1p9TJrsRqc/gviz/tq?tqx=out:json',
   '002': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSeMQGffph4J2g3_Bdi20QGGrxvsJlR3i3X1otdlzN2mm7-MyRjHEimz756K8b99id_h2xHZMLMnM6D/gviz/tq?tqx=out:json',
-  '003': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSXrPKpL-DdlabNMiX3LRhDIixxspH0QhVcV_btrBgD2NlSpUPaBn0RTlQdby2QZr1Eq_-YhE-v_dPt/gviz/tq?tqx=out:json',
+  '003': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRWeX5xPq2vV5DRZDRXoFhEFmlNoi6Sr3HF3Dk2WkC7fx2WkVPHWaF9C4Xp9a2RcCxIQxszHpI1FnTD/gviz/tq?tqx=out:json',
   '004': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSO9jBMI5xgbxV9-ovOzpm8hiwMrfPLrTj91Pv-qPPANAzi9I3WpCPRAJ98sXCG02q4uKrxDjxlzFp/gviz/tq?tqx=out:json',
   '005': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQgE9gnhIcb2lTWJxIP2VAsDPpdOXUcjGAK6s5YFIXdW-QGkvjm593zkYGBip9S5xIHUBjRCrp-5du5/gviz/tq?tqx=out:json',
   '201': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSSxtoIApsl8rwlz9_yRDIGk5vBofv14Y8jsQyINGhDIEqebTllfx2XNkfv1QtKHkMYN3fwTI6n96h3/gviz/tq?tqx=out:json',
