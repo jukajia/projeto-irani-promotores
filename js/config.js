476064878a0a486a10750993f4e1e113b489e5ad
Juka Produções <82520164+jukajia@users.noreply.github.com>
2025-05-02 15:11:29 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,16 +1,21 @@
-// js/config.js
-window.PLANILHAS = {
-  'gestor': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQU9GiNO40jd-ZQkU_vzLRfxYhf5kdpZL_BoLmbok9DjLzqYAqHfObnp4MPs2V_HN9ZbWBb4kCHQKfh/gviz/tq?tqx=out:json',
-  '001': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRVQpJV736U_gkzIGzbIdRk4sObA4so3fdj-Emr8WYvd5X20PXr4re_OtEP866H4_LbdJ1p9TJrsRqc/gviz/tq?tqx=out:json',
-  '002': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSeMQGffph4J2g3_Bdi20QGGrxvsJlR3i3X1otdlzN2mm7-MyRjHEimz756K8b99id_h2xHZMLMnM6D/gviz/tq?tqx=out:json',
-  '003': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRWeX5xPq2vV5DRZDRXoFhEFmlNoi6Sr3HF3Dk2WkC7fx2WkVPHWaF9C4Xp9a2RcCxIQxszHpI1FnTD/gviz/tq?tqx=out:json',
-  '004': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSO9jBMI5xgbxgV9-ovOzpm8hiwMrfPLrTj91Pv-qPPANAzi9I3WpCPRAJ98sXCG02q4uKrxDjxlzFp/gviz/tq?tqx=out:json',
-  '005': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQgE9gnhIcb2lTWJxIP2VAsDPpdOXUcjGAK6s5YFIXdW-QGkvjm593zkYGBip9S5xIHUBjRCrp-5du5/gviz/tq?tqx=out:json',
-  '201': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSSxtoIApsl8rwlz9_yRDIGk5vBofv14Y8jsQyINGhDIEqebTllfx2XNkfv1QtKHkMYN3fwTI6n96h3/gviz/tq?tqx=out:json',
-  '202': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTn070Oncl75qOTtDk0V2sd5tsQTc6O7YK4EhmaRpUMjI6G9eyH7G-OqxHE3008zXvo0SuzF4EoyHPg/gviz/tq?tqx=out:json',
-  '203': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsy0shwB9gbG1fsQGVXU77eL94dkzZsow1TXPyVmjYeWRYKR-WC_2ipsFAo2kAgmsOUS0LeoWAOpJ9/gviz/tq?tqx=out:json',
-  '204': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSvfd8rATUR6BQ0ENsWUrqOU7YXkNQPqe9IaJm0a32jnAWswQ-PdszJV2WJvoZmwLRdCQjtTTAhwxp9/gviz/tq?tqx=out:json'
+// Configuração centralizada
+window.PLANILHA_URL = 'https://docs.google.com/spreadsheets/d/12gGbuVZ47KiUZpzY4c6cO3j4usigBtgUpuL_dgRBkS8/gviz/tq?tqx=out:json';
+
+// Mapeamento de abas
+window.ABAS_PLANILHA = {
+  GESTOR: 'Gestor',
+  LOJAS: 'Todas as Lojas'
 };
 
-// Use the gestor spreadsheet by default
-window.PLANILHA_URL = window.PLANILHAS['gestor'];
+// Mapeamento de lojas
+window.LOJAS = {
+  '001': 'Brasil',
+  '002': 'Parque Verde',
+  '003': 'Floresta',
+  '004': 'Tancredo',
+  '005': 'Gourmet',
+  '201': 'Portí 1',
+  '202': 'Portí 2',
+  '203': 'Portí 3',
+  '204': 'Portí 4'
+};
