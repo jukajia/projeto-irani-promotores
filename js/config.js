57743068aa898d1ef5ae0eb455f01617c523b070
Juka Produções <82520164+jukajia@users.noreply.github.com>
2025-05-07 16:39:34 -0300

    Update and rename public/assets/js/config.js to js/config.js

---
+++ js/config.js
@@ -0,0 +1,15 @@
+// Configuração única da planilha
+window.PLANILHA_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQU9GiNO40jd-ZQkU_vzLRfxYhf5kdpZL_BoLmbok9DjLzqYAqHfObnp4MPs2V_HN9ZbWBb4kCHQKfh/pub?gid=0&single=true&output=json';
+
+// Mapeamento de nomes de lojas
+window.LOJAS = {
+  '001': 'Brasil',
+  '002': 'Parque Verde',
+  '003': 'Floresta',
+  '004': 'Tancredo',
+  '005': 'Gourmet',
+  '201': 'Portí 1',
+  '202': 'Portí 2',
+  '203': 'Portí 3',
+  '204': 'Portí 4'
+};
