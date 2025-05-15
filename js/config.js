0518ff88238f949e3d5bfda92b25ad26aa281c3d
Juka Produções <82520164+jukajia@users.noreply.github.com>
2025-05-15 01:24:15 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,4 @@
-// js/config.js
 window.PLANILHAS = {
-  'gestor': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQU9GiNO40jd-ZQkU_vzLRfxYhf5kdpZL_BoLmbok9DjLzqYAqHfObnp4MPs2V_HN9ZbWBb4kCHQKfh/pubhtml?gid=299822528&single=true&tqx=out:json',
-  'lojas': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQU9GiNO40jd-ZQkU_vzLRfxYhf5kdpZL_BoLmbok9DjLzqYAqHfObnp4MPs2V_HN9ZbWBb4kCHQKfh/pubhtml?gid=1632148302&single=true&tqx=out:json'
+  gestor: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQU9GiNO40jd-ZQkU_vzLRfxYhf5kdpZL_BoLmbok9DjLzqYAqHfObnp4MPs2V_HN9ZbWBb4kCHQKfh/pubhtml?gid=299822528&single=true&tqx=out:json',
+  lojas: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQU9GiNO40jd-ZQkU_vzLRfxYhf5kdpZL_BoLmbok9DjLzqYAqHfObnp4MPs2V_HN9ZbWBb4kCHQKfh/pubhtml?gid=1632148302&single=true&tqx=out:json'
 };
